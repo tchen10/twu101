f53a9d32a03078592754d0ebd2b55a174122aa20
Tiffany Chen <tchen@Tiffanys-MacBook-Pro.local>
2015-02-04 19:18:41 -0600

    first commit

---
+++ Chapter1.java
@@ -0,0 +1,18 @@
+import java.lang.Integer;
+
+public class Chapter1 {
+    public static void main (String[] args) {
+        //Print the sum of all odd numbers from 1 to 100
+        int num = 1;
+        int sum = 0;
+
+        while (num < 100) {
+            if (num % 2 == 1) {
+                sum = sum + num;
+            }
+            num += 1;
+        }
+
+        System.out.println(sum);
+    }
+}
